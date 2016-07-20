c90630715694c551f72c402dd288d79483047ba2
Andy Eckhardt <eckhardtandy@gmail.com>
2016-07-20 09:54:43 +0200

    Weitere Verbesserungen

--- task3/andy_eckhardt/time.rs
+++ task3/andy_eckhardt/time.rs
@@ -1,12 +1,13 @@
 
-fn getMessage(time: u8) -> String {
+fn get_message(time: u8) -> String {
        
     
     match time {
+        6...7 | 13...17 | 23...24 => format!("Hallo!"),
         8...12 => format!("Guten Morgen"),
         0...5 => format!("Warum bist du denn um {} noch wach ?", time),
         18...22 => format!("Guten Abend"),
-        _ => format!("Hallo!"),
+        _ => format!("Fehler"),
     }
 }
 
